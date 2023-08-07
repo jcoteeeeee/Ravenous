6da2c74a5c3e31d9dfa3fa81b0c9f08246bb8ce3
John Cote <johncote61598@gmail.com>
2023-08-07 14:41:06 -0400

    created Header componen, styled it and exported/imported to App

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'  
-import Business from './components/Business'
+import './App.css'    
+import Header from './components/Header'
+import Business from './components/Business' 
 
 function App() {
   return (
-    <>
+    <> 
+      <Header/> 
       <Business/> 
     </>
   )
